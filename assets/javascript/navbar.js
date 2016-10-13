d2f00a84d0b5debe82d1d24f937d2e60ed725136
Todd Germaine <trgermaine@gmail.com>
2016-10-13 02:04:52 -0400

    updated web files

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -30,12 +30,3 @@ var top = $('.top');
   });
 
   
-            // Masonry JQuery
-
-$('.grid').masonry({
-  // set itemSelector so .grid-sizer is not used in layout
-  itemSelector: '.grid-item',
-  // use element for option
-  columnWidth: '.grid-sizer',
-  percentPosition: true
-})
